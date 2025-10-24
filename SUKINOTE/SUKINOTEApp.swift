c120348cff5da4f4494a1bcdfa4545c5cb440c6e
kazmawed <harinezumi.camera@gmail.com>
2025-10-24 22:26:10 +0900

    Initial Commit

---
+++ SUKINOTE/SUKINOTEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUKINOTEApp.swift
+//  SUKINOTE
+//
+//  Created by Kazma Wed on 2025/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SUKINOTEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
