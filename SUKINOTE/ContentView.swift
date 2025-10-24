a4bfe1d1f6933f4dd44e3b97c760f2b059c4dbdd
kazmawed <harinezumi.camera@gmail.com>
2025-10-24 23:47:59 +0900

    feat: Implement count functionality and update UI to display a list

--- SUKINOTE/ContentView.swift
+++ SUKINOTE/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count = 0
+    
+    func countUp() {
+        count += 1
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(
+            0..<count,
+            id: \.self
+        ) { index in
+            HStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world! \(index + 1)")
+            }
         }
-        .padding()
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(
+                    action: {},
+                ) {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+        .toolbar {
+            ToolbarSpacer(.flexible, placement: .bottomBar)
+            ToolbarItem(placement: .bottomBar) {
+                Button(
+                    action: countUp,
+                ) {
+                    Image(systemName: "plus")
+                }
+            }
+        }
     }
 }
 
